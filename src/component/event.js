f4d701e8145f81ca5386a7aa20d08fd8fb3ff974
MorganNdungu <ndungumorgan20@gmail.com>
2023-05-03 21:51:24 +0300

    2nd commit

--- src/component/event.js
+++ src/component/event.js
@@ -1,11 +1,27 @@
-const Message= ()=>{
-    let message=()=>{
-        alert("he heey");
-    }
+// const Message= ()=>{
+//     let message=()=>{
+//         alert("he heey");
+//     }
+//     return(
+//         <div>
+//             <button onClick={()=>message()}>Mee</button>
+//         </div>
+//     );
+// }
+// export default Message;
+import { Component } from "react";
+class Message extends Component{
+   
+        Message=()=>hey
+    
+}
+render(){
     return(
         <div>
-            <button onClick={()=>message()}>Mee</button>
+            <h1>message: {this.state.message}</h1>
+                <button onClick={()=>this.message()}>mess</button>
         </div>
     );
 }
+
 export default Message;
