a0a89c57f2f326187de82f92e607237bd9d78a38
MorganNdungu <ndungumorgan20@gmail.com>
2023-05-07 12:11:23 +0300

    prac

--- src/component/map.js
+++ src/component/map.js
@@ -1,7 +1,20 @@
 const Map =()=>{
-    let price= [120 ,150,180, 90];
-    return(
-            price.map((price) = price * 0.1)
-    )
+let price= [120 ,150,180, 90];
+    
+           const increment =price.map((price) => price * 0.1);
+           let nowPrice =price.map((price)=>price + (increment));
+            return(
+                <div>
+                    <h1>increment on prices are: {increment}</h1>
+                    <h1>now price is: {nowPrice}</h1>
+                </div>
+            );
+        //  const nowPrice= price.map((increment)=> price + {increment})
+        //  return(
+        //     <div>
+        //         <h1>NOw prices are: {nowPrice}</h1>
+        //     </div>
+        //  );
+
 }
 export default Map;
